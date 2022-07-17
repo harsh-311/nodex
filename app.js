37d3dd8f83d88f427ee61045a6dab55c43386a52
harsh-311 <harshpatel3671@gmail.com>
2022-07-17 19:19:36 +0530

    remove mongo code

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use('/static', express.static('static'));
-const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/testharsh', { useNewUrlParser: true, useUnifiedTopology: true });
-// const contectSchema = new mongoose.Schema({
-//     name: String,
-//     email: String,
-//     Address: String,
-//     number: String
-// });
-// const Contect = mongoose.model('Contect', contectSchema);
+
 app.use(express.urlencoded())
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+app.get('/status', (req, res) => {
+    res.end('this our website verion:1.0');
+    // res.render('home');
+    // console.log("You are in website")
+})
+
 app.get('/', (req, res) => {
     // res.end('this our website');
     res.render('demo');
@@ -30,14 +29,8 @@ app.get('/content', (req, res) => {
     // console.log("You are in website")
 })
 app.post('/contect', (req, res) => {
-//    var mydata=new Contect(req.body);
-//    mydata.save().then(()=>{
+
     res.end("our data is store in database")
-//    }).catch(()=>{
-//     res.end("our data can not be store in database");
-//    })
-    // res.render('content');
-    
 })
 app.set("port", 3000);
 app.listen(3000, () => {
